3fa50b9237ccf68271ca18ae4db5101d8bae4237
Yu Chen Zhao <yczhao0916@gmail.com>
2021-05-02 12:15:53 -0400

    Attempting to fix scopes again

--- cogs/GoogleAPI.py
+++ cogs/GoogleAPI.py
@@ -10,9 +10,7 @@ class GoogleAPI(commands.Cog):
     #commands
     @commands.command(aliases = ['import'])
     async def importFromGoogle(self, ctx):
-        response = requests.get("https://event-reminder-discord-bot.herokuapp.com/authorize")
-        data = response.json()
-        await ctx.send(data['authorization_url'])
+        await ctx.send("hi")
 
 
 def setup(client):
