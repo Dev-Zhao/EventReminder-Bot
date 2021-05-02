104ccb786844c5e2ad3a9eb4a962ac65100f9cf6
Yu Chen Zhao <yczhao0916@gmail.com>
2021-05-01 21:30:56 -0400

    Change google verification method

--- GoogleAPI.py
+++ GoogleAPI.py
@@ -5,7 +5,7 @@ app= Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('google175fe4a0c26aaf2b.html')
+  return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
