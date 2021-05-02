fca2e923c18f563799ca791c0e38319a354c665d
Yu Chen Zhao <yczhao0916@gmail.com>
2021-05-02 18:43:06 -0400

    Fix fields

--- cogs/GoogleAPI.py
+++ cogs/GoogleAPI.py
@@ -45,8 +45,6 @@ class GoogleAPI(commands.Cog):
                     count += 1
                     if "summary" in event and "description" in event and "start" in event and "dateTime" in event["start"]:
                         embed_field = (f"\n -------| {event['summary']} |------- \n Time: {event['start']['dateTime']} - {event['end']['dateTime']} \n {event['description']}")
-                    else:
-                        embed_field = (f"\n -------| {event['summary']} |------- ")
                     embed.add_field(name=f"Event{count}", value=f"```{embed_field}```", inline=False)
                     if count == 5:
                         break
