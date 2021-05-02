eca173967394206e5889776f9c3953e42841d442
Yu Chen Zhao <yczhao0916@gmail.com>
2021-05-02 11:07:51 -0400

    Update client secrets

--- web.py
+++ web.py
@@ -10,7 +10,7 @@ import googleapiclient.discovery
 
 # This variable specifies the name of a file that contains the OAuth 2.0
 # information for this application, including its client_id and client_secret.
-CLIENT_SECRETS_FILE = "client_id.json"
+CLIENT_SECRETS_FILE = "client_secret_464056054365-n17ssc4td11rte9fgf8jb3v5vi7e9v9e.apps.googleusercontent.com.json"
 
 # This OAuth 2.0 access scope allows for full read/write access to the
 # authenticated user's account and requires requests to use an SSL connection.
